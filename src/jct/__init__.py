8678094f9af8c5c22bd9b0ae7e7cfe70b9871445
JosephOforkansi <oforkansi01@allegheny.edu>
2026-02-20 12:43:53 -0500

    new

--- src/jct/__init__.py
+++ src/jct/__init__.py
@@ -1,4 +1,4 @@
 # src/jct/__init__.py
-from .config import CONFIG
-from .chunking import extract_code_chunks
-from .search import embed_texts, search
+from .chunking import extract_chunks
+from .embedding import embed_texts
+from .search import search
